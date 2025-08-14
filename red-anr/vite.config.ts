86b11760a842cab9540c106c26f6c99378662a49
Paola Juarez Morales <paola.juarez.dev@gmail.com>
2025-08-13 21:50:54 -0400

    Ajuste de directorio base para vite

--- red-anr/vite.config.ts
+++ red-anr/vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/red-anr/',
   plugins: [react(), 
     tailwindcss(),],
   resolve: {
